c7b920f76eb6ad9c64f04050260c1649c03de114
Maksim Kostromin <daggerok@gmail.com>
2017-10-09 09:20:18 +0300

    update some eventsourcing - found bugs to fix

--- 09-user-management-es-cqrs/user-management/src/main/java/daggerok/domain/rest/UserCommandResource.java
+++ 09-user-management-es-cqrs/user-management/src/main/java/daggerok/domain/rest/UserCommandResource.java
@@ -27,30 +27,83 @@ public class UserCommandResource {
 
     val id = UUID.randomUUID();
     userRepository.save(new User(id));
-    return links(id);
+
+    return asList(
+        self(id),
+        activate(id)
+    );
   }
 
   @PostMapping("/{id}/activate")
   public List<Link> activateUserCommand(@PathVariable final UUID id) {
 
     val user = userRepository.find(id);
-    user.activate();
 
+    user.activate();
     userRepository.save(user);
-    return links(id);
+
+    return asList(
+        self(id),
+        changeNickname(id, "newNickname")
+    );
   }
 
   @SneakyThrows
-  private List<Link> links(final UUID id) {
+  @PostMapping("/{id}/change-nickname/{newNickname}")
+  public List<Link> changeNicknameCommand(@PathVariable final UUID id,
+                                          @PathVariable("newNickname") final String newNickname) {
+    val user = userRepository.find(id);
 
-    val activateCommand = UserCommandResource.class.getMethod("activateUserCommand", UUID.class);
-    val getUserQuery = UserQueryResource.class.getMethod("getUserQuery", UUID.class);
-    val replayUserEvents = UserQueryResource.class.getMethod("replayUserEvents", UUID.class);
+    user.changeNickname(newNickname);
+    userRepository.save(user);
 
     return asList(
-        linkTo(getUserQuery, id).withSelfRel(),
-        linkTo(activateCommand, id).withRel("activate"),
-        linkTo(replayUserEvents, id).withRel("replay")
+        self(id),
+        deactivate(id)
     );
+  }
+
+  @PostMapping("/{id}/deactivate")
+  public List<Link> deactivateUserCommand(@PathVariable final UUID id) {
+
+    val user = userRepository.find(id);
+
+    user.deactivate();
+    userRepository.save(user);
+
+    return asList(
+        self(id),
+        replay(id)
+    );
+  }
+
+  @SneakyThrows
+  private static Link self(final UUID id) {
+    val getUserQuery = UserQueryResource.class.getMethod("getUserQuery", UUID.class);
+    return linkTo(getUserQuery, id).withSelfRel();
+  }
+
+  @SneakyThrows
+  private static Link replay(final UUID id) {
+    val replayUserEvents = UserQueryResource.class.getMethod("replayUserEvents", UUID.class);
+    return linkTo(replayUserEvents, id).withRel("replay");
+  }
+
+  @SneakyThrows
+  private static Link activate(final UUID id) {
+    val activateCommand = UserCommandResource.class.getMethod("activateUserCommand", UUID.class);
+    return linkTo(activateCommand, id).withRel("activate");
+  }
+
+  @SneakyThrows
+  private static Link changeNickname(final UUID id, final String newNickname) {
+    val changeNicknameCommand = UserCommandResource.class.getMethod("changeNicknameCommand", UUID.class, String.class);
+    return linkTo(changeNicknameCommand, id, newNickname).withRel("change-nickname");
+  }
+
+  @SneakyThrows
+  private static Link deactivate(final UUID id) {
+    val deactivateCommand = UserCommandResource.class.getMethod("deactivateUserCommand", UUID.class);
+    return linkTo(deactivateCommand, id).withRel("deactivate");
   }
 }
