a601230f06b4d79f78e98bd47ed34c76dbbbfeca
Maksim Kostromin <daggerok@gmail.com>
2017-10-18 22:18:20 +0300

    secure actuator endpoints

--- jax-rs-security/src/main/java/daggerok/config/SpringSecurityConfig.java
+++ jax-rs-security/src/main/java/daggerok/config/SpringSecurityConfig.java
@@ -3,7 +3,9 @@ package daggerok.config;
 import lombok.val;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,7 +15,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurityConfig {
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
   static UserDetails user(final String username, final String... roles) {
 
@@ -30,5 +32,18 @@ public class SpringSecurityConfig {
         user("user", "USER")
     );
     return new InMemoryUserDetailsManager(db);
+  }
+
+  @Override
+  protected void configure(final HttpSecurity http) throws Exception {
+
+    http
+        .httpBasic()
+          .and()
+        .authorizeRequests()
+          .antMatchers("/application/**")
+            .hasRole("ADMIN")
+          .anyRequest()
+            .authenticated();
   }
 }
