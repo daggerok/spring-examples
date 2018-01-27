321865cfe901340b605957d09c57ea1160ce4fd8
Maksim Kostromin <daggerok@gmail.com>
2018-01-27 23:41:14 +0200

    add splettir-aggregator

--- http-integration-java-dsl/server/src/main/java/daggerok/HttpIntegrationJavaDslApplication.java
+++ http-integration-java-dsl/server/src/main/java/daggerok/HttpIntegrationJavaDslApplication.java
@@ -1,5 +1,6 @@
 package daggerok;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,11 +10,13 @@ import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.http.Http;
 import org.springframework.integration.splitter.DefaultMessageSplitter;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonMap;
+import static java.util.stream.Collectors.joining;
 import static org.springframework.http.HttpMethod.POST;
 
 @SpringBootApplication
@@ -40,28 +43,26 @@ public class HttpIntegrationJavaDslApplication {
   @Configuration
   static class MapperConfig {
 
-    /**
-     * http :8000/ k1=v1 k2=v2
-     * <p>
-     * <pre>
-     *    [
-     *     {
-     *       "keys": [
-     *         "k1",
-     *         "k2"
-     *       ]
-     *     },
-     *     {
-     *       "values": [
-     *         "v1",
-     *         "v2"
-     *       ]
-     *     }
-     *   ]
-     * </pre>
-     *
-     * @return flow
-     */
+    /*
+
+    http :8000/ k1=v1 k2=v2
+
+    [
+      {
+        "keys": [
+          "k1",
+          "k2"
+        ]
+      },
+      {
+        "values": [
+          "v1",
+          "v2"
+        ]
+      }
+    ]
+
+    */
 
     @Bean
     IntegrationFlow mapperFlow() {
@@ -77,23 +78,44 @@ public class HttpIntegrationJavaDslApplication {
     }
   }
 
+  @Slf4j
   @Configuration
   static class SplitterConfig {
+
+    /*
+
+    http post :8000/split-aggregate k=v kk=vv
+
+    [
+        "keys: k, kk",
+        "values: v, vv"
+    ]
+
+    */
 
     @Bean
     IntegrationFlow splitterFlow() {
 
-      return IntegrationFlows.from(Http.inboundGateway("/split")
+      return IntegrationFlows.from(Http.inboundGateway("/split-aggregate")
                                        .requestMapping(m -> m.methods(POST))
                                        .requestPayloadType(Map.class)
-                                          //.convertExceptions(true)
-                                       )
+                                     /*.convertExceptions(true)*/)
                              .<Map, List>transform(m -> asList(
                                  singletonMap("keys", m.keySet()),
                                  singletonMap("values", m.values())
                              ))
-                             .split(collectionSplitter()) // will split per items (not sutable for http to get fre responses)
-                             .aggregate() // will agregate back all together into response
+                             // will split into list of map per items (not sutable for http to get fre responses)
+                             .split(collectionSplitter())
+                             // do simple transformations..
+                             .<Map<String, Collection<String>>, String>transform(e -> e
+                                 .entrySet()
+                                 .stream()
+                                 .map(es -> es.getKey() + ": " + es.getValue()
+                                                                   .stream()
+                                                                   .collect(joining(", ")))
+                                 .collect(joining("; ")))
+                             // will agregate back all together into response
+                             .aggregate()
                              .get();
     }
 
